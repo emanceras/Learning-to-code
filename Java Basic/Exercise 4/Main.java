08623559b14cc8157dcba7a201831295261179b4
emanceras <121668965+emanceras@users.noreply.github.com>
2023-01-09 21:32:42 +0100

    Add files via upload

---
+++ Java Basic/Exercise 4/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+
+        System.out.println(cocheCrud);
+    }
+}
