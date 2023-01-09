ea97af646319e580cc6d92ee6e7e45be9f7add12
emanceras <121668965+emanceras@users.noreply.github.com>
2023-01-09 21:03:38 +0100

    Delete Básico/pruebas directory

--- Básico/pruebas/src/com/example/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Vehiculo Coche = new Vehiculo("Ford", "Mondeo", 2018, 60500,12300);
-
-        System.out.println(Coche.fabricante +"\n"+ Coche.modelo +"\n" + Coche.year +"\n" + Coche.km +"\n" + Coche.precio);
-    }
-}
